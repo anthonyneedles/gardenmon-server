f8c4c402c5ce1e1304ccb547296f30a2d957dc46
Anthony Needles <needles.anthony@gmail.com>
2024-03-31 12:08:47 -0700

    Add column selection, device filtering, and minutes to date query

--- gardenmon_server.py
+++ gardenmon_server.py
@@ -9,6 +9,12 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+VALID_COLUMNS = {"ambient_humidity", "ambient_light_lx", "ambient_temp_f", "cpu_temp_f", "soil_moisture_level", "soil_moisture_val", "soil_temp_f"}
+
+class Device(Enum):
+    gardenmon = "device = 'gardenmon'"
+    gardenmon_two = "device = 'gardenmon2'"
+
 class TimeGrouping(Enum):
     all_data = ""
     fifteen_min = "DATE_ADD(DATE_FORMAT(insert_time, '%Y-%m-%d %H'), INTERVAL FLOOR(MINUTE(insert_time) / 15) * 15 MINUTE)"
@@ -35,53 +41,59 @@ def query_data():
     start_date = request.args.get('start_date')
     end_date = request.args.get('end_date')
     grouping_period = request.args.get('grouping_period')
+    device = request.args.get("device")
+    columns = request.args.getlist("columns")
+    
+    if len(columns) > 0:
+        if not all(el in VALID_COLUMNS for el in columns):
+            return jsonify({"error": f"column must be in {VALID_COLUMNS}"})
+    else:
+        columns = list(VALID_COLUMNS)
+
+    try:
+        device = Device[device].value
+    except:
+        if device is not None:
+            return jsonify({"error": "device must either be unspecified, gardenmon, or gardenmon_two"})
+        device = "TRUE"
 
     if not start_date or not end_date or not grouping_period:
         return jsonify({"error": "must specify start_date, end_date, and grouping_period"}), 400
+
     try:
-        start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d-%H')
-        end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d-%H')
+        start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d-%H-%M')
+        end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d-%H-%M')
         if start_date >= end_date:
             return jsonify({"error": "start_date must be before end_date"}), 400
     except Exception as e:
-        return jsonify({"error": "Use date format YYYY-MM-DD-HH"}), 400
+        return jsonify({"error": "Use date format YYYY-MM-DD-HH-MM"}), 400
+
     try:
         time_grouping = TimeGrouping[grouping_period]
     except Exception as e:
-        print(e)
         return jsonify({"error": "grouping_period must be all_data, fifteen_min, hour, day, or month"})
 
     connection = create_db_connection()
     cursor = connection.cursor(dictionary=True)
     if time_grouping is TimeGrouping.all_data:
+        string_columns = ','.join(columns)
         query = f"""
             SELECT
-                ambient_humidity,
-                ambient_light_lx,
-                ambient_temp_f,
-                cpu_temp_f,
-                soil_moisture_level,
-                soil_moisture_val,
-                soil_temp_f,
+                {string_columns},
                 insert_time,
                 device
             FROM {local_options.database_table}
-            WHERE insert_time >= '{start_date}' AND insert_time < '{end_date}'
+            WHERE insert_time >= '{start_date}' AND insert_time < '{end_date}' and {device}
         """
     else:
+        avg_columns = ','.join([f"AVG({el}) as {el}" for el in columns])
         query = f"""
             SELECT
-                AVG(ambient_humidity) AS ambient_humidity,
-                AVG(ambient_light_lx) AS ambient_light_lx,
-                AVG(ambient_temp_f) AS ambient_temp_f,
-                AVG(cpu_temp_f) AS cpu_temp_f,
-                AVG(soil_moisture_level) AS soil_moisture_level,
-                AVG(soil_moisture_val) AS soil_moisture_val,
-                AVG(soil_temp_f) AS soil_temp_f,
+                {avg_columns},
                 {time_grouping.value} AS insert_time,
                 device
             FROM {local_options.database_table}
-            WHERE insert_time >= '{start_date}' AND insert_time < '{end_date}'
+            WHERE insert_time >= '{start_date}' AND insert_time < '{end_date}' and {device}
             GROUP BY {time_grouping.value}, device
         """
     cursor.execute(query)
@@ -89,10 +101,6 @@ def query_data():
     cursor.close()
     connection.close()
     return jsonify(rows), 200
-
-@app.route('/test', methods=['GET'])
-def test_query():
-    return "big gulps huh?\n\n\n\nwhelp, see ya later!\n"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=False)
