66b32d25b5eb6221e18aa4d191eb16cff7f7c9d0
Anthony Needles <needles.anthony@gmail.com>
2024-04-04 22:31:46 -0700

    moved query work to get_data()

--- gardenmon_server.py
+++ gardenmon_server.py
@@ -1,9 +1,10 @@
-import datetime
+from datetime import datetime
 from enum import Enum
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 import local_options
 import mysql.connector
+from typing import List
 
 app = Flask(__name__, static_folder='GardenMonWebsite')
 cors = CORS(app)
@@ -31,13 +32,72 @@ def create_db_connection():
         )
         if connection.is_connected():
             return connection
-    except Exception as e:
-        raise e
+        else:
+            raise RuntimeError
+    except Exception:
+        raise RuntimeError("database connection failed")
+
+def get_data(columns, device, start_date, end_date, grouping_period):
+    if len(columns) > 0:
+        if not all(el in VALID_COLUMNS for el in columns):
+            raise ValueError(f"columns must be in {VALID_COLUMNS}")
+    else:
+        columns = list(VALID_COLUMNS)
+    
+    if device:
+        try:
+            device = Device[device].value
+        except KeyError:
+            raise ValueError(f"device must either be unspecified or in {[device.name for device in Device]}")
+    else:
+        device = "TRUE"
+    
+    if not start_date or not end_date or not grouping_period:
+        raise ValueError("must specify start_date, end_date, and grouping_period")
+    try:
+        start_date = datetime.strptime(start_date, '%Y-%m-%d-%H-%M')
+        end_date = datetime.strptime(end_date, '%Y-%m-%d-%H-%M')
+    except ValueError:
+        raise ValueError("use date format: YYYY-MM-DD-HH-MM")
+    if start_date >= end_date:
+        raise ValueError("start_date must be before end_date")
+
+    try:
+        time_grouping = TimeGrouping[grouping_period]
+    except KeyError:
+        raise ValueError(f"grouping_period must be 'all_data', 'fifteen_min', 'hour', 'day', or 'month'")
+
+    with create_db_connection() as connection, connection.cursor(dictionary=True) as cursor:
+        if time_grouping is TimeGrouping.all_data:
+            string_columns = ','.join(columns)
+            query = f"""
+                SELECT
+                    {string_columns},
+                    UNIX_TIMESTAMP(insert_time) as insert_time,
+                    device
+                FROM {local_options.database_table}
+                WHERE insert_time >= '{start_date}' AND insert_time < '{end_date}' and {device}
+            """
+        else:
+            avg_columns = ','.join([f"AVG({el}) as {el}" for el in columns])
+            query = f"""
+                SELECT
+                    {avg_columns},
+                    UNIX_TIMESTAMP({time_grouping.value}) AS insert_time,
+                    device
+                FROM {local_options.database_table}
+                WHERE insert_time >= '{start_date}' AND insert_time < '{end_date}' and {device}
+                GROUP BY {time_grouping.value}, device
+            """
+
+        cursor.execute(query)
+        rows = cursor.fetchall()
+    
+    return rows
 
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
-
 
 @app.route('/data', methods=['GET'])
 @cross_origin()
@@ -48,62 +108,11 @@ def query_data():
     device = request.args.get("device")
     columns = request.args.getlist("columns")
     
-    if len(columns) > 0:
-        if not all(el in VALID_COLUMNS for el in columns):
-            return jsonify({"error": f"column must be in {VALID_COLUMNS}"})
-    else:
-        columns = list(VALID_COLUMNS)
-
     try:
-        device = Device[device].value
-    except:
-        if device is not None:
-            return jsonify({"error": "device must either be unspecified, gardenmon, or gardenmon_two"})
-        device = "TRUE"
-
-    if not start_date or not end_date or not grouping_period:
-        return jsonify({"error": "must specify start_date, end_date, and grouping_period"}), 400
-
-    try:
-        start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d-%H-%M')
-        end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d-%H-%M')
-        if start_date >= end_date:
-            return jsonify({"error": "start_date must be before end_date"}), 400
+        rows = get_data(columns, device, start_date, end_date, grouping_period)
     except Exception as e:
-        return jsonify({"error": "Use date format YYYY-MM-DD-HH-MM"}), 400
+        return jsonify({"error": str(e)}), 400
 
-    try:
-        time_grouping = TimeGrouping[grouping_period]
-    except Exception as e:
-        return jsonify({"error": "grouping_period must be all_data, fifteen_min, hour, day, or month"})
-
-    connection = create_db_connection()
-    cursor = connection.cursor(dictionary=True)
-    if time_grouping is TimeGrouping.all_data:
-        string_columns = ','.join(columns)
-        query = f"""
-            SELECT
-                {string_columns},
-                UNIX_TIMESTAMP(insert_time) as insert_time,
-                device
-            FROM {local_options.database_table}
-            WHERE insert_time >= '{start_date}' AND insert_time < '{end_date}' and {device}
-        """
-    else:
-        avg_columns = ','.join([f"AVG({el}) as {el}" for el in columns])
-        query = f"""
-            SELECT
-                {avg_columns},
-                UNIX_TIMESTAMP({time_grouping.value}) AS insert_time,
-                device
-            FROM {local_options.database_table}
-            WHERE insert_time >= '{start_date}' AND insert_time < '{end_date}' and {device}
-            GROUP BY {time_grouping.value}, device
-        """
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    cursor.close()
-    connection.close()
     return jsonify(rows), 200
 
 if __name__ == '__main__':
